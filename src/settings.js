3caeabc27869eebf45395a9cf3a7f6c1641865d1
Amazingh0rse <therahbek@gmail.com>
2021-01-15 17:00:21 +0100

    changes to admincrud

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,24 @@
+//Local
+
 export const URL = "http://localhost:8080/3_sem_eksamensprojekt";
 export const URLJokes = "http://localhost:8080/3_sem_eksamensprojekt/api/jokes";
 export const URLScrapeSequential = "http://localhost:8080/3_sem_eksamensprojekt/api/scrape/sequential";
 export const URLScrapeParallel = "http://localhost:8080/3_sem_eksamensprojekt/api/scrape/parallel";
 export const AllUsers = "http://localhost:8080/3_sem_eksamensprojekt/api/info/person/all";
 export const DeleteUser = "http://localhost:8080/3_sem_eksamensprojekt/api/info/delete/";
+export const UpdateUser = "http://localhost:8080/3_sem_eksamensprojekt/api/info/person";
+export const GetUser = "http://localhost:8080/3_sem_eksamensprojekt/api/info/person/";
+export const AddUser = "http://localhost:8080/3_sem_eksamensprojekt/api/info";
+
+
+/*
+//Live
+export const URL = "https://amazingh0rse.dk/Eksamen_Boilerplate_Backend/";
+export const URLJokes = "https://amazingh0rse.dk/Eksamen_Boilerplate_Backend/api/jokes";
+export const URLScrapeSequential = "https://amazingh0rse.dk/Eksamen_Boilerplate_Backend/api/scrape/sequential";
+export const URLScrapeParallel = "https://amazingh0rse.dk/Eksamen_Boilerplate_Backend/api/scrape/parallel";
+export const AllUsers = "https://amazingh0rse.dk/Eksamen_Boilerplate_Backend/api/info/person/all";
+export const DeleteUser = "https://amazingh0rse.dk/Eksamen_Boilerplate_Backend/api/info/delete/";
+export const UpdateUser = "https://amazingh0rse.dk/Eksamen_Boilerplate_Backend/api/info/person";
+export const GetUser = "https://amazingh0rse.dk/Eksamen_Boilerplate_Backend/api/info/person/";
+export const AddUser = "https://amazingh0rse.dk/Eksamen_Boilerplate_Backend/api/info";*/
